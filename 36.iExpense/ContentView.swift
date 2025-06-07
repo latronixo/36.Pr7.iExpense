d0e7cf5d96aebdc1a3989a57b220c7ed7c0654e6
Valentin <latronixo@gmail.com>
2025-06-07 18:13:47 +0300

    3/11 Демонстрация работы функции dismiss() для закрытия окна типа "Лист"

--- 36.iExpense/ContentView.swift
+++ 36.iExpense/ContentView.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
     let name: String
     var body: some View {
-        Text("Hello, \(name)!")
+        Button("Dismiss") {
+            dismiss()
+        }
     }
 }
 
