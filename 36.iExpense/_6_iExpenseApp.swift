fe4a4d8a5316db5c0698d79ffadf1cc34fa78f80
Valentin <latronixo@gmail.com>
2025-06-07 15:28:31 +0300

    Initial Commit

---
+++ 36.iExpense/_6_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_iExpenseApp.swift
+//  36.iExpense
+//
+//  Created by Валентин on 07.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct _6_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
