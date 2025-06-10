8f7b0dc8cc2f1f179adc3ecee814753bdac448f0
Valentin <latronixo@gmail.com>
2025-06-10 13:08:17 +0300

    4/11 Добавление и удаление элементов в List

--- 36.iExpense/ContentView.swift
+++ 36.iExpense/ContentView.swift
@@ -7,26 +7,28 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    let name: String
-    var body: some View {
-        Button("Dismiss") {
-            dismiss()
-        }
-    }
-}
-
 struct ContentView: View {
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
+        VStack {
+            List {
+                ForEach(numbers, id: \.self) {
+                    Text("Row \($0)")
+                }
+                .onDelete(perform: removeRows)
+            }
+            
+            Button("Add Number") {
+                numbers.append(currentNumber)
+                currentNumber += 1
+            }
         }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Valentin")
-        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
