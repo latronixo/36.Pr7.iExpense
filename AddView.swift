50fe407c17a908d4ee76b3b63752df065d869b4d
Valentin <latronixo@gmail.com>
2025-06-12 21:34:48 +0300

    9/11 Добавил окно в режиме sheet для добавления статьи расходов, которое вызывается по кнопке "+"

---
+++ AddView.swift
@@ -0,0 +1,40 @@
+//
+//  AddView.swift
+//  36.iExpense
+//
+//  Created by Валентин on 12.06.2025.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    var expenses: Expenses
+     
+    let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "RUB"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
